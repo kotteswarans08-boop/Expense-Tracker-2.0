8eeb92c1ff7f4d43109872829cf130f3f9c3046f
kotteswarans08-boop <kotteswaran.s08@gmail.com>
2026-05-12 01:01:02 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,24 @@
+const CACHE_NAME = 'etracker-v1';
+const ASSETS = [
+  './ETracker.html',
+  './manifest.json',
+  './icon.png'
+];
+
+// This part saves your app to the phone/computer memory
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// This part makes it work without internet
 self.addEventListener('fetch', (event) => {
-  // This can remain empty. 
-  // Its presence alone tells the browser the app is "Installable."
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
