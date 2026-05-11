2dec729ecdfa500ab0aa3898fac112c96a68adbd
kotteswarans08-boop <kotteswaran.s08@gmail.com>
2026-05-12 01:23:10 +0530

    sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,31 @@
-const CACHE_NAME = 'etracker-v3'; // Increased to v3 to force a fresh update
+const CACHE_NAME = 'etracker-v4'; // Bumped to v4
 const ASSETS = [
-  '/Expense-Tracker-2.0/',
-  '/Expense-Tracker-2.0/ETracker.html',
-  '/Expense-Tracker-2.0/manifest.json',
-  '/Expense-Tracker-2.0/expense_app_icon_512x512.png'
+  './',
+  './ETracker.html',
+  './manifest.json',
+  './expense_app_icon_512x512.png'
 ];
 
+// Install: Save files to cache
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting(); // Force the new service worker to take over immediately
+});
+
+// Activate: Clean up old caches
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
     })
   );
 });
 
+// Fetch: Serve from cache, then network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
